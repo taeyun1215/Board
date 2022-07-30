a1ab59f1bbd429cbbf18fd791eaac8f58ffd65ac
taeyun1215 <taeyun1215@naver.com>
2022-07-30 23:30:14 +0900

    front-end, back-end

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-//index.js
+// index.js
 
-var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
 
-app.get('/', function(req, res) { // '/' 위치에 'get'요청을 받는 경우,
-  res.send('Hello World!'); // "Hello World!"를 보냅니다.
+// DB setting
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
 });
 
-var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다. 
-app.listen(port, function(){ // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
-  console.log('server on! http://localhost:'+port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/posts', require('./routes/posts')); 
+
+// Port setting
+var port = 27017;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
 });
